cc14a739a3328f4377b298245f755b7668508340
Felipe Morales <felipe.morales_ex@scotiabank.cl>
2021-03-27 01:12:41 -0300

    added prepare script and fixed postinstall

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,3 +1,5 @@
 if (process.env.MOXY_GITHUB_INSTALL) {
-  require("child_process").exec("npm run build");
+  require("child_process").exec("npm run build", {
+    cwd: require("path").join(__dirname, ".."),
+  });
 }
